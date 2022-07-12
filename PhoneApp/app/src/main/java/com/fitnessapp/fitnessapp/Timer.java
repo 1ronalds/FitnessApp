6dbf37b1989675dbb406e109e47ecb1c86b2be65
Ronalds <ronaldsronix@gmail.com>
2022-07-12 13:43:19 +0300

    Timer improved

--- PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/Timer.java
+++ PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/Timer.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Timer extends AppCompatActivity {
+
+    public static final String EXTRA_SECONDS = "com.fitnessapp.fitnessapp.EXTRA_SECONDS";
+    public static final String EXTRA_MINUTES = "com.fitnessapp.fitnessapp.EXTRA_MINUTES";
 
 
     @Override
@@ -38,6 +42,49 @@ public class Timer extends AppCompatActivity {
     }
     public void openTimerStarted(View view) {
         Intent activity = new Intent(this, TimerStarted.class);
+        activity.putExtra(EXTRA_MINUTES, minutes);
+        activity.putExtra(EXTRA_SECONDS, seconds);
         startActivity(activity);
     }
+
+    int seconds = 0;
+    int minutes = 0;
+    public void addTime(View view){
+        String time = "";
+        String sSeconds = "";
+        TextView tw = (TextView) findViewById(R.id.textView6);
+        if(seconds+5 < 60){
+            seconds += 5;
+        } else {
+            seconds = 0;
+            minutes += 1;
+        }
+        if(seconds < 10) {
+            sSeconds = "0" + Integer.toString(seconds);
+        } else {
+            sSeconds = Integer.toString(seconds);
+        }
+        time = Integer.toString(minutes) + " : " + sSeconds;
+        tw.setText(time);
+    }
+    public void removeTime(View view){
+        String time = "";
+        String sSeconds = "";
+        TextView tw = (TextView) findViewById(R.id.textView6);
+        if(seconds-5 >= 0){
+            seconds -= 5;
+        } else if(seconds-5 == -5 && minutes > 0){
+            seconds = 55;
+            minutes -= 1;
+        }
+
+        if(seconds < 10) {
+            sSeconds = "0" + Integer.toString(seconds);
+        } else {
+            sSeconds = Integer.toString(seconds);
+        }
+        time = Integer.toString(minutes) + " : " + sSeconds;
+        tw.setText(time);
+    }
+
 }
--- PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/TimerStarted.java
+++ PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/TimerStarted.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class TimerStarted extends AppCompatActivity {
 
@@ -15,6 +16,19 @@ public class TimerStarted extends AppCompatActivity {
         setContentView(R.layout.activity_timer_started);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        Intent intent = getIntent();
+        int minutes = intent.getIntExtra(Timer.EXTRA_MINUTES, 0); //0 is mandatory, default value
+        int seconds = intent.getIntExtra(Timer.EXTRA_SECONDS, 0);
+        TextView tw = (TextView) findViewById(R.id.textView6);
+        String sSeconds = "";
+        String time = "";
+        if(seconds < 10) {
+            sSeconds = "0" + Integer.toString(seconds);
+        } else {
+            sSeconds = Integer.toString(seconds);
+        }
+        time = Integer.toString(minutes) + " : " + sSeconds;
+        tw.setText(time);
     }
     public void openTimer(View view){
         Intent activity = new Intent(this, Timer.class);

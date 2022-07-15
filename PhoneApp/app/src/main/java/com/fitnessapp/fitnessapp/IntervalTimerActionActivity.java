ede603d8e8f092e9f9c26c563daff4dc10737f97
Ruta Raudzina <rutaraudzina@gmail.com>
2022-07-15 14:59:44 +0300

    Fixed buttons layout at exercises section. Tried to implement pause button, but it doesn't work.

--- PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/IntervalTimerActionActivity.java
+++ PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/IntervalTimerActionActivity.java
@@ -1,6 +1,7 @@
 package com.fitnessapp.fitnessapp;
 
 import static java.lang.Integer.getInteger;
+import static java.lang.Integer.toBinaryString;
 import static java.lang.Integer.valueOf;
 
 import androidx.appcompat.app.ActionBar;
@@ -20,12 +21,11 @@ import org.w3c.dom.Text;
 public class IntervalTimerActionActivity extends AppCompatActivity {
     private CountDownTimer CDT;
     private CountDownTimer CDT2;
-    private boolean isTimerRunning;
+    public Button pauseStartTimer;
+    public Button restartTimer;
+    public boolean isTimerRunning;
     public int s;
     public int ss;
-
-    /*private Button buttonStartPause;
-    private Button buttonReset;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +54,13 @@ public class IntervalTimerActionActivity extends AppCompatActivity {
         int sec = wSec + rSec;
         TextView tw2 = (TextView) findViewById(R.id.textView3);
         TextView tw3 = (TextView) findViewById(R.id.textViewSets);
-
+        pauseStartTimer = findViewById(R.id.button6);
+        restartTimer = findViewById(R.id.button5);
 
         CDT = new CountDownTimer(wSec, 1000) {
+            @Override
             public void onTick(long l) {
+                isTimerRunning = true;
                 long wSec = l;
                 int min = (int) (wSec / 1000) / 60;
                 int sec = (int) (wSec / 1000) % 60;
@@ -72,14 +75,16 @@ public class IntervalTimerActionActivity extends AppCompatActivity {
                 toneGen1.startTone(ToneGenerator.TONE_CDMA_MED_PBX_L, 150);
                 if ((s-1)>0) {
                     CDT2 = new CountDownTimer(rSec, 1000) {
-
                         public void onTick(long l) {
+                            isTimerRunning = true;
                             long rSec = l;
                             int min = (int) (rSec / 1000) / 60;
                             int sec = (int) (rSec / 1000) % 60;
                             String visual = String.format("%02d:%02d", min, sec);
                             tw.setText(visual);
                             tw2.setText("Rest");
+
+
                         }
 
                         public void onFinish() {
@@ -101,29 +106,68 @@ public class IntervalTimerActionActivity extends AppCompatActivity {
                 }
             }
         }.start();
+
+        pauseStartTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isTimerRunning==true){
+                    CDT.cancel();
+                    isTimerRunning = false;
+                    pauseStartTimer.setText("▶");
+                }
+                else if(tw2.getText()=="Work"){
+                    pauseStartTimer.setText("II");
+                    isTimerRunning = true;
+                    CDT.start();
+                }
+                else {
+                    pauseStartTimer.setText("II");
+                    isTimerRunning = true;
+                    CDT2.start();
+                }
+            }
+        });
+
+        restartTimer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
     }
 
     public void openExercises(View view) {
         CDT.cancel();
+        CDT2.cancel();
         Intent activity = new Intent(this, Exercises.class);
         startActivity(activity);
     }
 
     public void openSettings(View view) {
         CDT.cancel();
+        CDT2.cancel();
         Intent activity = new Intent(this, SettingsActivity.class);
         startActivity(activity);
     }
 
     public void openIntervalTimer(View view){
         CDT.cancel();
+        CDT2.cancel();
         Intent activity = new Intent(this, IntervalTimerActivity.class);
         startActivity(activity);
     }
 
     public void openTimer(View view){
         CDT.cancel();
+        CDT2.cancel();
         Intent activity = new Intent(this, Timer.class);
+        startActivity(activity);
+    }
+
+    public void resetTimer(){
+        CDT.cancel();
+        CDT2.cancel();
+        Intent activity = new Intent(this, IntervalTimerActivity.class);
         startActivity(activity);
     }
 

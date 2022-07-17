b005762958bb6f2029f2e266bc5bbf6166986470
Ruta Raudzina <rutaraudzina@gmail.com>
2022-07-17 15:03:48 +0300

    Rewrote interval timer code, because it was written in a twisted way. Made the pause/start and reset buttons working.

--- PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/IntervalTimerActionActivity.java
+++ PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/IntervalTimerActionActivity.java
@@ -1,9 +1,5 @@
 package com.fitnessapp.fitnessapp;
 
-import static java.lang.Integer.getInteger;
-import static java.lang.Integer.toBinaryString;
-import static java.lang.Integer.valueOf;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,15 +18,13 @@ import org.w3c.dom.Text;
 
 public class IntervalTimerActionActivity extends AppCompatActivity {
     private CountDownTimer CDT;
-    private CountDownTimer CDT2;
     public Button pauseStartTimer;
     public Button restartTimer;
-    public boolean isTimerRunning;
-    public int s;
-    public int ss;
-    public long wSec;
-    public long rSec;
-    public long wwSec;
+    public boolean isTimerRunning = false;
+    public long residualTime;
+    public boolean isWorkTime = true;
+    public boolean isRestTime = false;
+    public int setsCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,124 +40,141 @@ public class IntervalTimerActionActivity extends AppCompatActivity {
         String theme = getSharedPreferences("Settings", Context.MODE_PRIVATE).getString("theme", null);
 
         TextView tw = findViewById(R.id.textView);
+        TextView tw2 = (TextView) findViewById(R.id.textView3);
+        TextView tw3 = (TextView) findViewById(R.id.textViewSets);
+        pauseStartTimer = findViewById(R.id.button6);
+        restartTimer = findViewById(R.id.button5);
 
         Intent intent = getIntent();
         final int sets = intent.getIntExtra("sets", 0);
         final int workSec = intent.getIntExtra("wSec", 0);
         final int restSec = intent.getIntExtra("rSec", 0);
 
-        final int secWork = valueOf(workSec);
-        final int secRest = valueOf(restSec);
-        wSec = secWork;
-        rSec = secRest;
-        s = valueOf(sets);
-        ss = s;
+        residualTime=workSec;
+        timer(residualTime, restSec, workSec, sets, tw, tw2, tw3);
 
-        TextView tw2 = (TextView) findViewById(R.id.textView3);
-        TextView tw3 = (TextView) findViewById(R.id.textViewSets);
-        pauseStartTimer = findViewById(R.id.button6);
-        restartTimer = findViewById(R.id.button5);
-
-        CDT = new CountDownTimer(wSec, 1000) {
-            @Override
-            public void onTick(long l) {
-                isTimerRunning = true;
-                wwSec = l;
-                int min = (int) (wwSec / 1000) / 60;
-                int sec = (int) (wwSec / 1000) % 60;
-                String visual = String.format("%02d:%02d", min, sec);
-                tw.setText(visual);
-                tw2.setText("Work");
-                tw3.setText("Set " + (ss + 1 - s));
-            }
-
-            public void onFinish() {
-                ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-                toneGen1.startTone(ToneGenerator.TONE_CDMA_MED_PBX_L, 150);
-                if ((s-1)>0) {
-                    CDT2 = new CountDownTimer(rSec, 1000) {
-                        public void onTick(long l) {
-                            isTimerRunning = true;
-                            long rSec = l;
-                            int min = (int) (rSec / 1000) / 60;
-                            int sec = (int) (rSec / 1000) % 60;
-                            String visual = String.format("%02d:%02d", min, sec);
-                            tw.setText(visual);
-                            tw2.setText("Rest");
-                        }
-                        public void onFinish() {
-                            ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-                            toneGen1.startTone(ToneGenerator.TONE_CDMA_MED_PBX_L, 150);
-                            tw2.setText("Rest");
-                            s--;
-                            if (s > 0) CDT.start();
-                            else {
-                                tw.setText("Well done!");
-                                tw2.setText("Rest");
-                            }
-                        }
-                    }.start();
-                }
-                else{
-                    tw.setText("Well done!");
-                    tw2.setText("Rest");
-                }
-            }
-        }.start();
-
-        pauseStartTimer.setOnClickListener(new View.OnClickListener() {
+        restartTimer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isTimerRunning==true){
-                    CDT.cancel();
-                    isTimerRunning = false;
-                    pauseStartTimer.setText("▶");
-                }
-                else if(tw2.getText()=="Work"){
-                    pauseStartTimer.setText("II");
-                    isTimerRunning = true;
-                    CDT.start();
-                }
-                else {
-                    pauseStartTimer.setText("II");
-                    isTimerRunning = true;
-                    CDT2.start();
-                }
+                resetTimer(restSec, workSec, sets, tw, tw2, tw3);
             }
         });
+
     }
 
+    public void timer(long allTime, int restSec, int workSec, int sets, TextView tw, TextView tw2, TextView tw3){
+        if (residualTime>0 && isWorkTime == true && setsCount<=sets) {
+            setsCount+=1;
+            isTimerRunning = true;
+            CDT = new CountDownTimer(residualTime, 1000) {
+                @Override
+                public void onTick(long l) {
+                    long wSec = l;
+                    int min = (int) (wSec / 1000) / 60;
+                    int sec = (int) (wSec / 1000) % 60;
+                    String visual = String.format("%02d:%02d", min, sec);
+                    tw.setText(visual);
+                    tw2.setText("Work");
+                    tw3.setText("Set " + setsCount + "/" + sets);
+                    residualTime -= 1000;
+                }
+
+                @Override
+                public void onFinish() {
+                    ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                    toneGen1.startTone(ToneGenerator.TONE_CDMA_MED_PBX_L, 150);
+                    isWorkTime = false;
+                    isRestTime = true;
+                    if(setsCount==sets){
+                        isTimerRunning = false;
+                        tw.setText("Well done!");
+                        tw2.setText("Rest");
+                    }
+                    else {
+                        isWorkTime = false;
+                        isRestTime = true;
+                        residualTime = restSec;
+                        timer(allTime, restSec, workSec, sets, tw, tw2, tw3);
+                    }
+                }
+            }.start();
+        }
+        else if (residualTime>0 && isRestTime==true && setsCount<sets){
+            isTimerRunning = true;
+            CDT = new CountDownTimer(residualTime, 1000) {
+                @Override
+                public void onTick(long l) {
+                    long rSec = l;
+                    int min = (int) (rSec / 1000) / 60;
+                    int sec = (int) (rSec / 1000) % 60;
+                    String visual = String.format("%02d:%02d", min, sec);
+                    tw.setText(visual);
+                    tw2.setText("Rest");
+                    tw3.setText("Set " + setsCount + "/" + sets);
+                    residualTime -= 1000;
+                }
+
+                @Override
+                public void onFinish() {
+                    ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                    toneGen1.startTone(ToneGenerator.TONE_CDMA_MED_PBX_L, 150);
+                    isWorkTime = true;
+                    isRestTime = false;
+                    if(setsCount>=sets){
+                        isTimerRunning = false;
+                        tw.setText("Well done!");
+                        tw2.setText("Rest");
+                    }
+                    else {
+                        residualTime=workSec;
+                        isWorkTime = true;
+                        isRestTime = false;
+                        timer(allTime, restSec, workSec, sets, tw, tw2, tw3);
+                    }
+                }
+            }.start();
+        }
+    }
+
+    public void pauseStartTimer(View view){
+        if(isTimerRunning = true){
+            CDT.cancel();
+            isTimerRunning = false;
+            pauseStartTimer.setText("▶");
+        }
+        else{
+            CDT.start();
+            pauseStartTimer.setText("II");
+        }
+    }
+
+    public void resetTimer(int restSec, int workSec, int sets, TextView tw, TextView tw2, TextView tw3){
+        isWorkTime = true;
+        isRestTime = false;
+        residualTime = workSec;
+        setsCount = 1;
+        CDT.start();
+    }
 
     public void openIntervalTimer(View view){
         CDT.cancel();
-        CDT2.cancel();
         Intent activity = new Intent(this, IntervalTimerActivity.class);
         startActivity(activity);
     }
 
     public void openTimer(View view){
         CDT.cancel();
-        CDT2.cancel();
         Intent activity = new Intent(this, Timer.class);
-        startActivity(activity);
-    }
-
-    public void resetTimer(View view){
-        CDT.cancel();
-        CDT2.cancel();
-        Intent activity = new Intent(this, IntervalTimerActivity.class);
         startActivity(activity);
     }
 
     public void openExercises(View view){
         CDT.cancel();
-        CDT2.cancel();
         Intent activity = new Intent(this, Exercises.class);
         startActivity(activity);
     }
     public void opeSettings(View view){
         CDT.cancel();
-        CDT2.cancel();
     }
 
 }

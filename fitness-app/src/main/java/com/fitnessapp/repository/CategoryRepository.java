3ab536e075cb0f2f252d49345107ad4af284a6e1
Ronalds <ronaldsronix@gmail.com>
2022-07-18 22:39:57 +0300

    deleted accidentally created folders

--- fitness-app/src/main/java/com/fitnessapp/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fitnessapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fitnessapp.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-
-}
--- fitness-app/src/main/java/com/fitnessapp/repository/ExerciseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fitnessapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fitnessapp.entity.Exercise;
-
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
-
-}

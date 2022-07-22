82b4b6247b3ec2e1803b6ea0ab7c425f99f85410
Ronalds <ronaldsronix@gmail.com>
2022-07-22 11:41:34 +0300

    bug fixes

--- PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/Timer.java
+++ PhoneApp/app/src/main/java/com/fitnessapp/fitnessapp/Timer.java
@@ -251,7 +251,7 @@ public class Timer extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
                 textView2 = ((TextView) adapterView.getChildAt(0));
-                if(theme.equals("dark"){
+                if(theme.equals("dark")){
                     textView2.setTextColor(Color.parseColor("#FFFFFF"));
                 } else {
                     textView2.setTextColor(Color.parseColor("#000000"));
